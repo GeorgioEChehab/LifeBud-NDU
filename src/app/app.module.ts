068e00aaa55562fee1904d292fff496171100be6
GEORGIOECHEHAB <gechehab@ndu.edu.lb>
2022-12-21 21:15:58 +0200

    edit data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+            IonicStorageModule.forRoot({
+              name: 'mydatabase'
+            })],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, LocalNotifications],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+const STORAGE_KEY = 'mylist';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService 
+{
 
-  constructor() { }
+  constructor(private storage: Storage) 
+  {
+    this.init();
+
+  }
+
+  init()
+  {
+    this.storage.create();
+
+  }
+
+  getData()
+  {
+    return this.storage.get(STORAGE_KEY) || [];
+
+  }
+
+  async add(item: any)
+  {
+    const storedData = await this.storage.get(STORAGE_KEY) || [];
+    storedData.push(item);
+    return this.storage.set(STORAGE_KEY, storedData);
+
+  }
+
+  async remove(index: any)
+  {
+    const storedData = await this.storage.get(STORAGE_KEY) || [];
+    storedData.splice(index, 1);
+    return this.storage.set(STORAGE_KEY, storedData);
+
+  }
 }
