63a5f846b2f310c596f7ab65d16157711c411d4b
GEORGIOECHEHAB <gechehab@ndu.edu.lb>
2023-01-16 22:26:44 +0200

    added 0.5 of settings code and generated 2 pages

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -9,19 +9,30 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class SettingsPage implements OnInit 
 {
+  income: number = 0;
+
   constructor(private data_service: DataService, private alert_controller: AlertController) 
   {
-
   }
 
-  async deleteIncome()
+  async test()
   {
-    if(this.data_service.get_income == 0)
+    
+  }
+
+  //START adjustIncome()
+  async adjustIncome() //user can adjust his income
+  {
+    this.data_service.getAmount('income');
+    this.income = this.data_service.get_income;
+
+    if((this.income == 0) || (this.income == null))
     {
       const alert = await this.alert_controller.create(
         {
-          header: 'No Income To Be Deleted',
-          cssClass: 'delete-income-alert',
+          header: 'Error!',
+          message: 'No Income To Be Adjusted',
+          cssClass: 'delete-income-error-alert',
           buttons: [{text: 'OK', cssClass: 'ok-button'}]
 
         });
@@ -29,11 +40,136 @@ export class SettingsPage implements OnInit
       await alert.present();
     }
     else
-      if(this.data_service.get_income > 0)
+      if(this.income > 0)
       {
-        
+        const alert = await this.alert_controller.create(
+          {
+            header: 'Please Enter Your New Income',
+            cssClass: 'add-income-alert',
+            inputs:[
+              {
+                name: 'income',
+                type: 'number'
+              }
+            ],
+            buttons: [
+              {
+                text: 'Cancel',
+                role: 'cancel',
+                cssClass: 'cancel-button'
+              },
+              {
+                text: 'Submit',
+                cssClass: 'submit-button',
+                handler: async (alertData) => {
+                  this.income = alertData.income;
+                  if(this.income > 0)
+                  {
+                    this.data_service.setAmount('income', this.income);
+    
+                    const alert = await this.alert_controller.create({
+                      cssClass: 'add-income-submit-alert',
+                      message: 'Income Updated',
+                      buttons: [{
+                        text: 'OK',
+                        cssClass: 'ok-button'
+    
+                      }],
+                    });
+                
+                    await alert.present();
+                  }
+                  else
+                    if(this.income < 0)
+                    {
+                      const alert = await this.alert_controller.create({
+                        header: 'Error!',
+                        cssClass: 'error-income-alert',
+                        subHeader: 'Invalid Income',
+                        message: 'New Income Cannot Be Less Than Zero',
+                        buttons: [{
+                          text: 'OK',
+                          cssClass: 'ok-button',
+                          handler: () => {
+                            this.adjustIncome();
+                          }
+                        }],
+                      });
+                  
+                      await alert.present();
+                    }
+                  else
+                    if(this.income == 0)
+                    {
+                      const alert = await this.alert_controller.create({
+                        header: 'Error!',
+                        cssClass: 'error-income-alert',
+                        subHeader: 'Invalid Income',
+                        message: 'New Income Cannot Be Equal To Zero',
+                        buttons: [{
+                          text: 'OK',
+                          cssClass: 'ok-button',
+                          handler: () => {
+                            this.adjustIncome();
+                          }
+                        }],
+                      });
+                  
+                      await alert.present();
+                    }
+                    
+                    
+                }
+              }
+            ]
+    
+          });
+        await alert.present();
       }
   }
+  //END adjustIncome()
+  
+  //--------------------------------------------------------------------------------------------------------------------------------
+  
+  //START deleteIncome()
+  async deleteIncome() //deletes user's previously added income
+  {
+    this.data_service.getAmount('income');
+    this.income = this.data_service.get_income;
+    if((this.income == 0) || (this.income == null))
+    {
+      const alert = await this.alert_controller.create(
+        {
+          header: 'Error!',
+          message: 'No Income To Be Deleted',
+          cssClass: 'delete-income-error-alert',
+          buttons: [{text: 'OK', cssClass: 'ok-button'}]
+
+        });
+
+      await alert.present();
+    }
+    else
+      if(this.income > 0)
+      {
+        this.data_service.removeAmount('income');
+        this.income = 0;
+
+        const alert = await this.alert_controller.create({
+          cssClass: 'delete-income-alert',
+          header: 'Income Deleted',
+          buttons: [{
+            text: 'OK',
+            cssClass: 'ok-button'
+
+          }],
+        });
+    
+        await alert.present();
+
+      }
+  }
+  //END deleteIncome()
 
   ngOnInit() {
   }
