c8716d700b9ac4ffceae0a88e1498e9395485966
GEORGIOECHEHAB <gechehab@ndu.edu.lb>
2023-03-02 19:34:24 +0200

    add contribute

--- src/app/pages/contribute/contribute.page.ts
+++ src/app/pages/contribute/contribute.page.ts
@@ -9,8 +9,26 @@ import { Storage } from '@ionic/storage-angular';
 })
 export class ContributePage implements OnInit 
 {
+  conversionRates: number[] = [82000, 80600, 81600, 81500, 81800, 81800, 85200, 80000, 81300];
+  predictedRate: number = -99;
 
-  constructor(private nav_controller: NavController, private storage: Storage) 
+  predictRate() 
+  {
+    console.log('START');
+    let sum = 0;
+    for (let i = 0; i < this.conversionRates.length - 1; i++) {
+      sum += this.conversionRates[i];
+    }
+    const averageRate = sum / (this.conversionRates.length - 1);
+    console.log(`1 ${this.predictedRate}`);
+    this.predictedRate = averageRate + (averageRate - this.conversionRates[this.conversionRates.length - 2]);
+    console.log(`2 ${this.predictedRate}`);
+    console.log(' ');
+    console.log('END');
+  }
+
+
+  constructor() 
   { 
 
   }
